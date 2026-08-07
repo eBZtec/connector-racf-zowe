b6044df694fa14b0bb2503dbe4b6f87441143587
Nikolas Corrêa <nikolas.correa@ebz.tec.br>
2026-08-07 11:40:14 -0300

    build: atualização de dependências para scan SCA Veracode fixes #1

---
+++ src/test/java/com/evolveum/polygon/connector/racf/rest/TestOpIT.java
@@ -0,0 +1,27 @@
+package com.evolveum.polygon.connector.racf.rest;
+
+import org.identityconnectors.common.security.GuardedString;
+import org.identityconnectors.framework.api.ConnectorFacade;
+import org.identityconnectors.framework.common.exceptions.ConnectionFailedException;
+import org.testng.annotations.Test;
+
+public class TestOpIT extends BaseIT{
+
+    @Test
+    public void valid() {
+        ConnectorFacade connector = setupConnector();
+        connector.test();
+    }
+
+    @Test(expectedExceptions = ConnectionFailedException.class)
+    public void invalidUrl() {
+        RacfRestConfiguration config = new RacfRestConfiguration();
+        config.setUrl("http://localhost:8092");
+        config.setUsername("user");
+        config.setPassword(new GuardedString("user".toCharArray()));
+
+        ConnectorFacade connector = setupConnector(config);
+        connector.test();
+    }
+
+}
